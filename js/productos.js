84cfceaca3ce85615dc7e811a269cfc7263d2ec8
Bgimnz <bgimnzo@gmail.com>
2023-11-19 22:28:53 -0300

    Sprint 3 - Entrega Final Proyecto

--- js/productos.js
+++ js/productos.js
@@ -1,6 +1,78 @@
-// Obetengo los datos por JSON
-const getProducts = async () => {
-    const respuesta = await fetch ("../pages/productos.json");
-    const datos = await respuesta.json()
-};
+const catalogo =  [
+        {
+            "id": 1,
+            "title": "Broches Arcoiris",
+            "descrip": "x12 Broches Muticolor",
+            "estado": "Disponible",
+            "categoria": "marca",
+            "img": "../img-productos/broches.png"
+        },
+        {
+            "id": 2,
+            "title": "Jabón Dove",
+            "descrip": "Jabón Cremoso Original",
+            "estado": "Disponible",
+            "categoria": "marca",
+            "img": "../img-productos/dove-jabon.png"
+        },
+        {
+            "id": 3,
+            "title": "Papel Higienico Elegante",
+            "descrip": "Papel Higienico 4 x 80 m",
+            "estado": "No Disponible",
+            "categoria": "papeles",
+            "img": "../img-productos/elegante-papel-higienico.jpg"
+        },
+        {
+            "id": 4,
+            "title": "Pala de Mano Marlim",
+            "descrip": "Pala de Plastico Reforzada",
+            "estado": "No Disponible",
+            "categoria": "marca",
+            "img": "../img-productos/pala-plastico.png"
+        },
+        {
+            "id": 5,
+            "title": "Jabón Seiseme",
+            "descrip": "Jabón Blanco SEISEME",
+            "estado": "Disponible",
+            "categoria": "quimico",
+            "img": "../img-productos/seiseme.png"
+        },
+        {
+            "id": 6,
+            "title": "Jabón Liquido Tipo ALA",
+            "descrip": "Bidon de 5L Tipo ALA",
+            "estado": "Disponible",
+            "categoria": "liquidos",
+            "img": "../img-productos/bidon-tipo-ala.jpg"
+        },
+];
 
+
+let item = document.getElementById("catal");
+
+catalogo.map((producto) => {
+
+    item.innerHTML+= `
+    <div class="item" key=${producto.id}>
+    <img src=${producto.img} class="itg" alt=${producto.title} />
+    <div class="info-producto">
+    <h5 class="titu-product">${producto.title}</h5>
+    <p class="descripcion">${producto.descrip}</p>
+    </div>
+    <button class="estado">${producto.estado}</button>
+    </div>
+    `
+}
+)
+
+
+
+//<div class="${producto.categoria}"></div>
+//necesito que java ponga la clase a cada producto segun su categoria y asi pueda ser filtrado por el otro script
+
+
+/*const filtrarProducto = (categoria) => {
+    const updatedList = catalogo.filter((x)=> x.categoria === categoria);
+}*/
--- readme.md
+++ readme.md
@@ -1,32 +1,32 @@
-#Proyecto Página BA Multiplica 2.0
+# Proyecto Página BA Multiplica 2.0 👨‍💻
 
-##Descripción
+## Descripción ✒️
 
 Esta es mi primer página la cual trata sobre un Local que vende Artículos de Limpieza, Perfumería y Química. Es un tipo de página que busca mostrar el Local y que los clientes puedan descubrirlo por medio de internet de forma cómoda, para poder contactarse, encontrarlo en el mapa y descubrir los productos disponibles en el momento. 
 
-##Funcionalidades
+## Funcionalidades ➕
 
 Los visitantes pueden contactarse para obtener más información y tener una atención personalizada por medio de WhatsApp, Teléfono y/o Instagram a la hora de buscar productos.
 Pueden visualizar el catálogo de nuestros productos con un solo click. 
 También pueden dejarnos su información al pie de la página donde lo contactaremos a la brevedad por medio de un Mail. 
 
-##Gracias
+## Gracias 🍵
 
 Justamente eso, gracias por conocer nuestro local y por elegirnos. Encuéntrenos en Dirección 123 y compre todo lo que necesite para abastecerse de productos de limpieza para su hogar. 
 Nosotros tenemos los mejores precios del barrio y la mejor atención. Nos encontrara como lo muestra la página a la Mañana de 9:30 a 13:30  y a la Tarde de 16:30 a 20:30. ¡Los Esperamos!
 
-##Link
+## Link 📎
 
 Podra ver el Proyecto en vivo a traves del siguiente enlace:
 
 https://bgimnz.github.io/CursoProgramacion-BAM2/
 
-##Hecho con 
+## Hecho con 🪸
 
 * HTML
 * CSS
 * JAVASCRIPT
 
-##Autor
+## Autor 🚬
 
 _Gomez Braian BAM 2.0_
